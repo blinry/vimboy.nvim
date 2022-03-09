4b566cb80142b9752a088466a00ad9fb7276afdd
blinry <mail@blinry.org>
2022-03-09 14:46:02 +0100

    Use new nvim_set_hl

--- syntax/vimboy.lua
+++ syntax/vimboy.lua
@@ -1,11 +1,11 @@
-vim.cmd('highlight link vimboyLink Underlined')
+vim.api.nvim_set_hl(0, "vimboyLink", {link = "Underlined"})
 vim.cmd('syntax clear vimboyLink')
 
 local file_name = vim.api.nvim_buf_get_name(0)
 local base_dir = file_name:match(".*/")
 
-function addLink(filename)
+function AddLink(filename)
     vim.cmd('syntax match vimboyLink /\\c\\V\\<' .. vim.fn.escape(filename, '/\\') .. '/')
 end
 
-for _, file in ipairs(vim.fn.readdir(base_dir)) do addLink(file) end
+for _, file in ipairs(vim.fn.readdir(base_dir)) do AddLink(file) end
