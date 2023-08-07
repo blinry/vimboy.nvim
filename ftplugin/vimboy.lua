9b3a249bed49b65a0a63e4917ab62da06481b1d7
blinry <mail@blinry.org>
2023-08-07 17:21:35 +0200

    Add comments to functions

--- ftplugin/vimboy.lua
+++ ftplugin/vimboy.lua
@@ -1,7 +1,9 @@
-function Deb(text)
+-- Used for debugging purposes during development.
+function Debug(text)
     vim.cmd("unsilent echom '" .. text .. "'")
 end
 
+-- Open the link under the cursor, or the word under the cursor if no link is found.
 function OpenLinkUnderCursor()
     local cursor_col = vim.fn.col('.')
     if IsOnLink(cursor_col) then
@@ -16,31 +18,7 @@ function OpenLinkUnderCursor()
     end
 end
 
-function OpenVisualSelection()
-    vim.cmd('normal! ')
-    local minCol = vim.fn.col("'<")
-    local maxCol = vim.fn.col("'>")
-    local fileToOpen = string.sub(vim.fn.getline('.'), minCol, maxCol)
-    OpenPage(fileToOpen)
-end
-
-function OpenPage(name)
-    local file_name = vim.api.nvim_buf_get_name(0)
-    local base_dir = file_name:match(".*/")
-
-    local fileToOpen = name
-
-    if vim.fn.filereadable(name) == 0 then
-        for _, file in ipairs(vim.fn.readdir(base_dir)) do
-            if string.lower(name) == string.lower(file) then
-                fileToOpen = file
-                break
-            end
-        end
-    end
-    vim.cmd('edit ' .. vim.fn.fnameescape(fileToOpen))
-end
-
+-- Loop through the characters of the current line, until we're no longer on a link.
 function FindEndOfLink(start_col, end_col, diff)
     for col = start_col, end_col, diff do if not IsOnLink(col) then return col - diff end end
     return end_col
@@ -50,6 +28,34 @@ end
 function IsOnLink(col)
     local cursor_line = vim.fn.line('.')
     return #vim.fn.synstack(cursor_line, col) > 0
+end
+
+-- Open the current visual selection.
+function OpenVisualSelection()
+    vim.cmd('normal! ')
+    local minCol = vim.fn.col("'<")
+    local maxCol = vim.fn.col("'>")
+    local fileToOpen = string.sub(vim.fn.getline('.'), minCol, maxCol)
+    OpenPage(fileToOpen)
+end
+
+-- Open the page with the given name.
+function OpenPage(name)
+    local file_name = vim.api.nvim_buf_get_name(0)
+    local base_dir = file_name:match(".*/")
+
+    local fileToOpen = name
+
+    if vim.fn.filereadable(name) == 0 then
+        -- If the file doesn't exist, try to find versions of the file with different casing.
+        for _, file in ipairs(vim.fn.readdir(base_dir)) do
+            if string.lower(name) == string.lower(file) then
+                fileToOpen = file
+                break
+            end
+        end
+    end
+    vim.cmd('edit ' .. vim.fn.fnameescape(fileToOpen))
 end
 
 vim.keymap.set('n', '<CR>', OpenLinkUnderCursor)
